f74e96a37e341ef47a6d8f7f99e5344d6c0cdef8
guiravi <guillermo.ramirezvillalta@ucr.ac.cr>
2020-10-30 04:55:25 -0600

    Se cambian de nombre los métodos de insert a set

--- Event.py
+++ Event.py
@@ -3,7 +3,7 @@ class Event:
     def __init__(self, message, arrival_time, id_event):
         self.message = message              # Mensaje
         self.arrival_time = arrival_time    # Tiempo en el que ocurre el evento
-        self.id_event = id_event                    # Identificado de evento
+        self.id_event = id_event            # Identificado de evento
 
     def get_arrival_time(self):
         return self.arrival_time
