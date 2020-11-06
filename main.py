5bfd179fffe290cb93a9076eaa4ca24f8e7e3fe7
guiravi <guillermo.ramirezvillalta@ucr.ac.cr>
2020-11-06 02:41:26 -0600

    Se hacen cambios para poder guardar los datos de las distribuciones

--- main.py
+++ main.py
@@ -1,21 +1,43 @@
 from Computer import Computer
-from Message import Message
 from Event import Event
 
 # VARIABLES GLOBALES
+distribution_list = []
 event_queue = [Event]  # Cola de eventos en la que se ingresa cada uno de los eventos del sistema
 clock = 0.0  # Reloj del sistema
 computer_1 = Computer
 computer_2 = Computer
 computer_3 = Computer
 
-
-def test():
-    print("Test")
-
+distri = { 1 : "Normal método directo",
+           2 : "Normal método de la convolución",
+           3 : "Uniforme",
+           4 : "Exponencial",
+           5 : "Densidad"}
 
 # GENERADORES DE DISTRIBUCIONES
-def generate_d1():
+def generate_time(distribution):
+    i = distribution - 1
+    result = 0.0
+    if distribution_list[i] == 0:
+        print("Normal directo")
+        result = normal_direct()
+    elif distribution_list[i] == 1:
+        print("Normal convolución")
+        result = generate_d2()
+    elif distribution_list[i] == 2:
+        print("uniforme")
+        result = generate_d3()
+    elif distribution_list[i] == 3:
+        print("exponencial")
+        result = generate_d4()
+    elif distribution_list[i] == 4:
+        print("densidad")
+        result = generate_d5()
+    return result
+
+
+def normal_direct():
     return 0
 
 
@@ -63,9 +85,10 @@ def LMC1(event):
     # Se realiza el salto del reloj al inicio del evento
     clock = event.get_arrival_time()
 
-    # Se verifica si el procesador está ocupado
+    # Se verifica si el procesador está vacío
     if computer_1.get_processor(0).empty():
-        time = generate_d6()  # Se genera el tiempo aleatorio según la distribución D6
+        # Se genera el tiempo aleatorio según la distribución D6
+        time = generate_time(computer_1.get_processor(0).get_output_distribution())
         event.message.insert_departure_time(clock + time)  # Se ingresa cuando se terminara de procesar el mensage
         event_new = Event(event.get_message(), clock + time, 1)  # Se crea el evento SMC1
         insert_event(event_new)  # Se inserta el evento en la cola de eventos
@@ -79,13 +102,14 @@ def SMC1(event):
     return
 
 
+#MÉTODOS DE SIMULACIÓN
 def initialize(d1, d2, d3, d4, d5, d6):
     global computer_1
     global computer_2
     global computer_3
 
     # Se añaden las computadoras con cada una de sus respectivas distribuciones
-    computer_1 = Computer(-1)
+    computer_1 = Computer(0)
     computer_2 = Computer(1)
     computer_3 = Computer(4)
 
@@ -114,16 +138,16 @@ def simulation(amount_simulations, time, d1, d2, d3, d4, d5, d6):
         clock = 0.0
         while clock < time:
             # se busca el mínimo
-            i = get_min()
+            index = get_min()
 
             # Se extrae el evento de la cola de eventos
-            event = event_queue.pop(i)
+            event = event_queue.pop(index)
 
             # Se busca la ejecución que corresponde al evento y se ejecuta
             if event.type == 0:
-                LMC1(event)
+                LMC1(event)     # Se ejecuta el evento LMC1
             elif event.type == 1:
-                SMC1(event)
+                SMC1(event)     # Se ejecuta el evento SMC1
             elif event.type == 2:
                 print("option 3")
             elif event.type == 3:
@@ -131,5 +155,15 @@ def simulation(amount_simulations, time, d1, d2, d3, d4, d5, d6):
     return
 
 
+def create_distributions(i):
+    d1 = int(input("Ingrese la distribución D%s: " % (i)))
+    while not (0 < d1 < 6):
+        d1 = int(input("\t Ingrese un número de distribución válida por favor: "))
+    print("\t Usted eligió: %s para la distribución d%s" % (distri[d1], i))
+
+
 if __name__ == '__main__':
-    test()
+    # Se guarda el valor de cada una de las distribuciones
+    for i in range(1,7):
+        create_distributions(i)
+    check_simulation()
