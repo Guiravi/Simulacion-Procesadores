47a9dd72e2c00fd450823c58883bd0a2c4db42f6
KewQiu <7310376+KewQiu@users.noreply.github.com>
2020-11-10 16:50:19 -0600

    Correcciones

--- Distribution.py
+++ Distribution.py
@@ -73,10 +73,11 @@ class Distribution:
     
     # --------------- FIN Definición de Métodos Get y Set para atributos de clase Processor --------------- #
 
-    """
-    Se envía a calcular la distribución correspondiente y se retorna el valor obtenido 
-    """
     def calculate(self):
+        """
+        Método de clase
+        Se envía a calcular la distribución correspondiente y se retorna el valor obtenido 
+        """
         result = 0.0
         if (self.id == "Direct"):
             result = self.calculate_direct()
@@ -90,20 +91,22 @@ class Distribution:
             result = self.calculate_kx()
         return result
 
-    """
-    Se realiza el calculo de distribución normal método directo
-    """
     def calculate_direct(self):
+        """
+        Método de clase
+        Se realiza el calculo de distribución normal método directo
+        """
         x = -1
         while x < 0:  # Se verifica que el valor de x no sea negativo
             z = (math.sqrt(-2*math.log(random.random()))*math.cos(2*math.pi*random.random()))
             x = self.miu + math.sqrt(self.sigma2)*z
         return x
 
-    """
-    Se realiza el calculo de distribución normal convolusión
-    """
     def calculate_TLC(self):
+        """
+        Método de clase
+        Se realiza el calculo de distribución normal convolución
+        """
         x = -1
         while x < 0:  # Se verifica que el valor de x no sea negativo
             R = 0.0
@@ -113,34 +116,40 @@ class Distribution:
             x = self.miu + math.sqrt(self.sigma2)*z
         return x
 
-    """
-    Se realiza el calculo de distribución uniforme
-    """
     def calculate_uniform(self):
+        """
+        Método de clase
+        Se realiza el calculo de distribución uniforme
+        """
         return self.a + (self.b - self.a)*random.random()
 
 
-    """
-    Se realiza el calculo de distribución exponencial
-    """
     def calculate_exponential(self):
+        """
+        Método de clase
+        Se realiza el calculo de distribución exponencial
+        """
         rand = 0.0
-        while rand == 0:  # Se verifica que el aleatorio no sea 0
+        while rand == 0.0:  # Se verifica que el aleatorio no sea 0
             rand = random.random()
-        return ((-1)/(self.lambd))*math.log(random.random())
+        return ((-1)/(self.lambd))*math.log(rand)
 
 
-    """
-    Se realiza el calculo de distribución de función de densidad
-    """
     def calculate_kx(self):
+        """
+        Método de clase
+        Se realiza el calculo de distribución de función de densidad
+        """
         x = self.a - 1
         while not (self.a <= x <= self.b):  # Se verifica que x esté en el rango
             x = math.sqrt(((2*random.random())/(self.k)) + self.a**2)
         return x
 
-"""
-Se realiza el calculo de distribución uniforme para obtener rangos entre [a,b], 
-"""
 def calculate_uniform(a, b):
+    """
+    Método general
+    Se realiza el calculo de distribución uniforme para obtener rangos entre [a,b]
+    Los parámetros a y b los recibe de otros métodos de la simulación. 
+    Este método no se utiliza para la entrada de usuario, para esto existe el Método de Clase
+    """
     return a + (b - a)*random.random()
